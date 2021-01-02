da2220b92a0682e11daa324043ac8afee7dd303a
rajann44 <rajann44@gmail.com>
2021-01-02 18:25:00 +0100

    Add product search functionality

---
+++ cypress/pages/Page2.js
@@ -0,0 +1,24 @@
+class ContactPage {
+    elements = {
+        nameInput: '[data-qa="name"]',
+        emailInput: '[data-qa="email"]',
+        subjectInput: '[data-qa="subject"]',
+        messageInput: '[data-qa="message"]',
+        submitButton: '[data-qa="submit-button"]',
+        successMessage: '.alert-success'
+    }
+
+    submitContactForm(name, email, subject, message) {
+        cy.get(this.elements.nameInput).type(name);
+        cy.get(this.elements.emailInput).type(email);
+        cy.get(this.elements.subjectInput).type(subject);
+        cy.get(this.elements.messageInput).type(message);
+        cy.get(this.elements.submitButton).click();
+    }
+
+    verifySuccessMessage() {
+        cy.get(this.elements.successMessage).should('be.visible');
+    }
+}
+
+export default new ContactPage();
