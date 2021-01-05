f78eba3324f7a2bc0bc8423b49c2c4eaf88cfb93
rajann44 <rajann44@gmail.com>
2021-01-05 14:13:00 +0100

    Add product search functionality

---
+++ cypress/e2e/test_4.cy.js
@@ -0,0 +1,11 @@
+describe('Contact Form Tests', () => {
+    it('should submit contact form', () => {
+        cy.visit('/contact_us');
+        cy.get('[data-qa=name]').type('Test User');
+        cy.get('[data-qa=email]').type('test@example.com');
+        cy.get('[data-qa=subject]').type('Test Subject');
+        cy.get('[data-qa=message]').type('Test Message');
+        cy.get('[data-qa=submit-button]').click();
+        cy.get('.alert-success').should('be.visible');
+    });
+});
