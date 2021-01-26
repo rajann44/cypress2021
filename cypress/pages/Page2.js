a39e948e8b564b7c637a6bc24143ff541934491b
rajann44 <rajann44@gmail.com>
2021-01-26 09:28:00 +0100

    Add screenshot capture

--- cypress/pages/Page2.js
+++ cypress/pages/Page2.js
@@ -34,3 +34,4 @@ export default new ContactPage();
 // Commit on 2021-01-19 11:30:00: 26468
 // Commit on 2021-01-20 16:00:00: 24850
 // Commit on 2021-01-25 11:25:00: 15554
+// Commit on 2021-01-26 09:28:00: 21802
