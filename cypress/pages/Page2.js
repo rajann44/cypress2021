6601dc30888fdc33cb9f67741bffcba8f1443e84
rajann44 <rajann44@gmail.com>
2021-08-20 18:35:00 +0200

    Add API test cases

--- cypress/pages/Page2.js
+++ cypress/pages/Page2.js
@@ -133,3 +133,4 @@ export default new ContactPage();
 // Commit on 2021-08-15 09:04:00: 32629
 // Commit on 2021-08-15 11:12:00: 8876
 // Commit on 2021-08-16 11:18:00: 9960
+// Commit on 2021-08-20 18:35:00: 30146
