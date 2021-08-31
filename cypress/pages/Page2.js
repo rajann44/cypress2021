b36406936ac624c6fde6076666c0fbe936471e2b
rajann44 <rajann44@gmail.com>
2021-08-31 16:10:00 +0200

    Update page objects

--- cypress/pages/Page2.js
+++ cypress/pages/Page2.js
@@ -142,3 +142,4 @@ export default new ContactPage();
 // Commit on 2021-08-28 13:19:00: 6809
 // Commit on 2021-08-29 09:47:00: 12666
 // Commit on 2021-08-30 12:06:00: 32464
+// Commit on 2021-08-31 16:10:00: 21084
