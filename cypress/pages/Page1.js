f113d34a18858914d8b380c168cb2af9600c97a6
rajann44 <rajann44@gmail.com>
2021-09-13 15:04:00 +0200

    Add contact form tests

--- cypress/pages/Page1.js
+++ cypress/pages/Page1.js
@@ -143,3 +143,4 @@ export default new CartPage();
 // Commit on 2021-09-07 15:36:00: 29585
 // Commit on 2021-09-09 15:00:00: 8446
 // Commit on 2021-09-11 11:49:00: 928
+// Commit on 2021-09-13 15:04:00: 14289
