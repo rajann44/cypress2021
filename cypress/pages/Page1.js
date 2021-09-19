e275e00d6286d019e01f57f5796406e1b050548c
rajann44 <rajann44@gmail.com>
2021-09-19 18:31:00 +0200

    Improve test stability

--- cypress/pages/Page1.js
+++ cypress/pages/Page1.js
@@ -146,3 +146,4 @@ export default new CartPage();
 // Commit on 2021-09-13 15:04:00: 14289
 // Commit on 2021-09-14 12:09:00: 2009
 // Commit on 2021-09-15 09:57:00: 24149
+// Commit on 2021-09-19 18:31:00: 20343
