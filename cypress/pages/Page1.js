5734ddd35d4507587d0a4f1d4b1862a7bf083024
rajann44 <rajann44@gmail.com>
2021-09-22 12:34:00 +0200

    Add test reporting

--- cypress/pages/Page1.js
+++ cypress/pages/Page1.js
@@ -149,3 +149,4 @@ export default new CartPage();
 // Commit on 2021-09-19 18:31:00: 20343
 // Commit on 2021-09-19 13:39:00: 2287
 // Commit on 2021-09-20 09:10:00: 31430
+// Commit on 2021-09-22 12:34:00: 28193
