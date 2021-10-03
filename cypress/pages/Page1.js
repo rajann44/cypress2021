bb13c6291f0c4ea9c7c3c8561d0c685afb551d80
rajann44 <rajann44@gmail.com>
2021-10-03 15:59:00 +0200

    Improve error handling

--- cypress/pages/Page1.js
+++ cypress/pages/Page1.js
@@ -152,3 +152,4 @@ export default new CartPage();
 // Commit on 2021-09-22 12:34:00: 28193
 // Commit on 2021-09-29 11:36:00: 28172
 // Commit on 2021-10-01 10:09:00: 17828
+// Commit on 2021-10-03 15:59:00: 15368
