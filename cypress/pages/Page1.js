5c81e23e55c27c9e97c6aba0bb4e2bf18bc116d6
rajann44 <rajann44@gmail.com>
2021-12-30 09:42:00 +0100

    Add user registration tests

--- cypress/pages/Page1.js
+++ cypress/pages/Page1.js
@@ -200,3 +200,4 @@ export default new CartPage();
 // Commit on 2021-12-22 10:55:00: 12375
 // Commit on 2021-12-23 14:43:00: 10863
 // Commit on 2021-12-25 15:11:00: 2922
+// Commit on 2021-12-30 09:42:00: 24153
